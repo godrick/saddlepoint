65f973c7ccbbf0947a202e610c15e9438621bab5
godrick <godrickmaradona@gmail.com>
2025-09-12 17:24:47 -0700

    Add @examples to PoissonCGF and PoissonModelCGF roxygen docs

--- R/C-PoissonCGF.R
+++ R/C-PoissonCGF.R
@@ -20,6 +20,11 @@
 #'
 #' Ready-to-use CGF for Poisson. Accepts scalar or vector `lambda`.
 #' If `length(tvec)` is a multiple of `length(lambda)`, evaluation proceeds (iidReps="any").
+#'
+#' @examples
+#' # Evaluate K at t = 0.1 for lambda = 2
+#' # PoissonCGF$K(0.1, 2)
+#'
 #' @export
 PoissonCGF <- .make_univariate_model_cgf_matrix(
   K_elem = .poisson_K, K1_elem = .poisson_K1, K2_elem = .poisson_K2,
@@ -40,6 +45,18 @@ PoissonCGF <- .make_univariate_model_cgf_matrix(
 #' @param iidReps Either `"any"` or a positive integer. Default `"any"`.
 #' @param ... Passed through to the CGF creation (e.g., to override operators).
 #' @return A `CGF` object.
+#'
+#' @examples
+#' # ex 1: i.i.d. scenario for univariate Poisson(rate = 2)
+#' ex_iid <- PoissonModelCGF(lambda = function(x) x[1])
+#' # OR ex_iid <- PoissonModelCGF(lambda = function(x) x[1], iidReps = 3)
+#' ex_iid$K1(rep(0,3), 2)
+#'
+#' # ex 2: non-identical scenario with replication: lambda returns c(2,5), iidReps=3
+#' ex_repeat <- PoissonModelCGF(lambda = adaptor(indices = 1:2), iidReps = 3)
+#' # OR ex_repeat <- PoissonModelCGF(lambda = function(x) c(2,5) )
+#' ex_repeat$K1(rep(0,6), c(2,5))
+#'
 #' @export
 PoissonModelCGF <- function(lambda, iidReps = "any", ...) {
   .check_iidReps(iidReps)  # only "any" or positive integer
@@ -57,4 +74,3 @@ PoissonModelCGF <- function(lambda, iidReps = "any", ...) {
 
   adaptCGF(cgf = base, adaptor = lambda_fn)
 }
-
